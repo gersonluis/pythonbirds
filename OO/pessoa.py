5af88b1a74ad0fd16a030a0e6c7962912c21e676
gerson <gersoneich@gmail.com>
2019-03-10 18:45:43 -0300

    criado atributo nome e idade

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,10 +1,15 @@
 class Pessoa():
+    def __init__(self, nome=None, idade=49):
+        # criando primeiro atributo
+        self.idade = idade
+        self.nome = nome
     # criamos o primeiro metodo
     def cumprimentar(self):
         return f"Olá{id(self)}"
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa("Gerson")
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome, p.idade)
